ed75055660a3b5933605c1a08df75196ec61c4fd
Ngalim Siregar <ngalim.siregar@gmail.com>
2017-04-10 22:27:09 +0900

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var gulp = require('gulp'),
 gulp.task('jekyll', function() {
     return gulp.src('index.html', { read: false })
         .pipe(shell([
-            'bundle exec JEKYLL_ENV=production jekyll build'
+            'JEKYLL_ENV=production bundle exec jekyll build'
         ]));
 });
     
