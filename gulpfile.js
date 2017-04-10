2eacc3a3ff577cf212cff266371342fca562efff
Ngalim Siregar <ngalim.siregar@gmail.com>
2017-04-11 08:02:40 +0900

    log all content

--- gulpfile.js
+++ gulpfile.js
@@ -97,10 +97,8 @@ gulp.task('algolia-index', function() {
     return gulp.src('_site/algolia.json')
         .pipe(algoliasearch(algoliaAPPID, algoliaAPIKEY)
             .initIndex(algoliaINDEX)
-                .addObjects('_site/algolia.json', function(err, obj) {
-                    if(err) {
-                        console.error(err);
-                    }
+                .addObjects('_site/algolia.json', function(err, content) {
+                    console.log(content);
                 })
         )
         .pipe(gulp.dest('_site/'));
