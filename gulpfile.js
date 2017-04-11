1e56cc8a444a636c82df5082f7370c8de5b23131
Ngalim Siregar <ngalim.siregar@gmail.com>
2017-04-11 09:10:11 +0900

    update index

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task('optimize-html', function() {
 gulp.task('algolia-index', function() {
     let client = algoliasearch(algoliaAPPID, algoliaAPIKEY);
     let index = client.initIndex(algoliaINDEX)
-    return gulp.src('_site/algolia.json')
+    gulp.src('_site/algolia.json')
         .pipe(index.saveObjects('algolia.json', function(err, content) {
             console.log(content);
         }))
